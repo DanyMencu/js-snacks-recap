1329982e2088496bd98d1f222e3fa2afdd58a8e9
DanyMencu <dany69men@gmail.com>
2021-10-28 18:18:10 +0200

    Afternoon exercise (broken)

---
+++ Pomeriggio/script.js
@@ -0,0 +1,22 @@
+/* Ogni volta che si clicca un bottone, il nostro programma chiederà un numero di 4 cifre all’utente.
+Se l’utente ha inserito un numero corretto, allora il programma calcolerà la somma e le media di tutte le cifre che lo compongono stampandola nel DOM, altrimenti ripeterà la richiesta di inserimento finché non avrà inserito un numero corretto. */
+
+//Ref
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+    let userNumber = prompt('Scrivere un numero di almeno 4 cifre');
+    let somma = 0;
+    let media = 0;
+
+    while (userNumber.length != 4 || isNaN(userNumber)) {
+        userNumber = prompt('Scrivere un numero di almeno 4 cifre')
+    }
+
+    for (let i = 0; i < userNumber.length; 1++) {
+        somma += parseInt(userNumber[i]);
+    }
+    media = somma / numero.length;
+
+    console.log(somma , media);
+})
